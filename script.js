55b106fa13ddabde050857136d7f9cc14aa5cba6
PeeWee <oscar.oses.1127@gmail.com>
2020-07-06 18:14:55 -0400

    added some styles and created early quiz div layout

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
+//Query Selectors
 var timeEl = document.querySelector(".timer");
 var mainEl = document.querySelector("main");
 
 // Global variable
 var secondsLeft = 60;
 
-function setTime() {
-        
-}
