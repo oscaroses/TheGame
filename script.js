0257d771835d1140b6939751d40c50b38bd1c4e1
PeeWee <oscar.oses.1127@gmail.com>
2020-07-13 19:54:59 -0400

    fixed question

--- script.js
+++ script.js
@@ -1,78 +1,80 @@
 //Query Selectors
 var timeEl = document.querySelector(".timer");
 var mainEl = document.querySelector("#main");
-var startBut = document.querySelector(".button")
-var gameQs = document.querySelector("#theGame")
+var startBut = document.querySelector("#button1");
 
-var secondsLeft = 60;
+var secondsLeft = 120;
 
 const trivia = [
-    {
+  {
     question: "What is JavaScript?",
-    answers: ["A programming language", "A coffee drink", "A videogame", "A computer"],
-    correctAnswer: 0
-    },
-    {
+    answers: [
+      "A programming language",
+      "A coffee drink",
+      "A videogame",
+      "A computer",
+    ],
+    correctAnswer: 0,
+  },
+  {
     question: "What does DOM stand for?",
-    answers: ["Display of Monitor", "Document Object Model", "Department of Machines", "Delivery of Modal"],
-    correctAnswer: 1
-    },
-    {
+    answers: [
+      "Display of Monitor",
+      "Document Object Model",
+      "Department of Machines",
+      "Delivery of Modal",
+    ],
+    correctAnswer: 1,
+  },
+  {
     question: "Which file type do you link the .JS file to?",
     answers: [".html", ".css", ".jpg", ".com"],
-    correctAnswer: 0
-    },
-    {
+    correctAnswer: 0,
+  },
+  {
     question: "How do you declare an array?",
     answers: ["{}", "()", "<>", "[]"],
-    correctAnswer: 3
-    }
-]
+    correctAnswer: 3,
+  },
+];
 
 // When start button is clicked, display first question
 function startGame() {
-    updateDisplay()
-    setTime()
+  setTime();
+  updateDisplay();
 }
 
 // populate another question, when answer is checked.
-function checkanswer () {
-    
-}
+function checkanswer() {}
 
 function listenForAnswer() {
-
+  alert("hi");
 }
 
 function setTime() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--
-        timeEl.textContent = "Time:" + secondsLeft
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = "Time:" + secondsLeft;
 
-        if (secondsLeft <= 0) {
-            clearInterval(timerInterval)
-        }
-    }, 1000)
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
 }
 
 //populate question in main div
-function updateDisplay() {    
-gameQs.innerHTML = "";
-
-for (var i = 0; i < trivia.length; i++)
-var questions = trivia[i]
-
-var li = document.createElement("li");
-li.textContent = "questions"
-li.setAttribute("data-index", i);
-
-li.appendChild(button);
-theGame.appendChild(li);
-
+function updateDisplay() {
+  var quests = document.createElement("p");
+  quests.textContent = trivia[0].question;
+  trivia[0].answers.forEach((answer) => {
+    var para = document.createElement("button");
+    para.setAttribute("id", "buttons");
+    para.textContent = answer;
+    quests.appendChild(para);
+  });
+  mainEl.appendChild(quests);
 }
 
-
-startBut.addEventListener("click", function(event) {
-    event.preventDefault();
-    startGame();
-    })
+startBut.addEventListener("click", function () {
+  startGame();
+});
