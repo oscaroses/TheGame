1603f8d399013efed22df11fd144c3e1384a58fc
PeeWee <oscar.oses.1127@gmail.com>
2020-07-13 22:54:37 -0400

    started over. added labels. unable to populate questions without divs :/

---
+++ questions.js
@@ -0,0 +1,34 @@
+var questions = [
+  {
+    question: "What is JavaScript?",
+    option1: "A programming language",
+    option2: "A coffee drink",
+    option3: "A videogame",
+    option4: "A computer",
+    answer: "1",
+  },
+  {
+    question: "What does DOM stand for?",
+    option1: "Display of Monitor",
+    option2: "Document Object Model",
+    option3: "Department of Machines",
+    option4: "Delivery of Modal",
+    answer: "2",
+  },
+  {
+    question: "Which file type do you link the .JS file to?",
+    option1: ".html",
+    option2: ".css",
+    option3: ".jpg",
+    option4: ".com",
+    answer: "1",
+  },
+  {
+    question: "How do you declare an array?",
+    option1: "{}",
+    option2: "()",
+    option3: "<>",
+    option4: "[]",
+    answer: "4",
+  },
+];
